359cfd6a975b474aab44b31aef19bc1470f7b111
aryanair99 <a7nair@ucsd.edu>
2020-02-20 11:43:46 -0800

    done

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -11,3 +11,7 @@ $(document).ready(function() {
 function initializePage() {
 	// your code here
 }
+
+function e(){
+	ga("send", "event", "like", "click");
+}
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,11 @@ var projects = require('../projects.json');
  * GET home page.
  */
 
+exports.viewAlt = function(req, res){
+ 	projects["viewAlt"] = false;
+ 	res.render('index', projects);
+};
+
 exports.view = function(request, response){
   	response.render('index', projects);
 };
